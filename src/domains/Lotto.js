d0a0c33d3ecb8b1bb7d9fe08d44e32a2e0bad523
sooyeoniya <tndus1306403@naver.com>
2024-11-18 00:01:13 +0900

    test: Lotto 클래스 테스트 구현

--- src/domains/Lotto.js
+++ src/domains/Lotto.js
@@ -1,7 +1,7 @@
 import validateWinningNumbers from '../validations/validateWinningNumbers.js';
 
 /**
- * 과제에서 미리 주어진 LottoTest의 테스트 케이스에 맞춰 Lotto 클래스에 유효성 검사를 진행하도록 함
+ * @description 과제에서 미리 주어진 LottoTest의 테스트 케이스에 맞춰 Lotto 클래스에 유효성 검사를 진행하도록 함
  */
 class Lotto {
   /** @type {Array<number>} */ #winningNumbers;
