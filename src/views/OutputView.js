77978ccdd4f7ef06aed1c5fbb0a257653dfb5849
sooyeoniya <tndus1306403@naver.com>
2024-11-17 18:02:29 +0900

    feat(OutputView): 출력 요구사항 구현

---
+++ src/views/OutputView.js
@@ -0,0 +1,37 @@
+import { Console } from '@woowacourse/mission-utils';
+
+const OutputView = {
+  /**
+   * 사용자가 발행한 로또 번호를 출력한다.
+   * @param {Array<number>} lottoNumbers 
+   */
+  printIssuedLottoNumbers(lottoNumbers) {
+    Console.print(`[${lottoNumbers.join('')}]`);
+  },
+
+  printWinningStatisticsPrefix() {
+    Console.print('당첨 통계\n---\n');
+  },
+
+  /**
+   * 각 로또 당첨 등수 별 당첨 개수를 출력한다.
+   * @param {Map<number, number>} winningStatistics 
+   */
+  printWinningStatistics(winningStatistics) {
+    // Console.print(`3개 일치 (5,000원) - ${winningStatistics.get(3)}개`);
+    // Console.print(`4개 일치 (50,000원) - ${winningStatistics.get(4)}개`);
+    // Console.print(`5개 일치 (1,500,000원) - ${winningStatistics.get(5)}개`);
+    // Console.print(`5개 일치, 보너스 볼 일치 (30,000,000원) - ${winningStatistics.get(5.5)}개`);
+    // Console.print(`6개 일치 (2,000,000,000원) - ${winningStatistics.get(6)}개')`);
+  },
+
+  /**
+   * 총 수익률을 출력한다.
+   * @param {number} earningRate 
+   */
+  printEarningRate(earningRate) {
+    Console.print(`총 수익률은 ${earningRate}%입니다.`);
+  },
+}
+
+export default OutputView;
